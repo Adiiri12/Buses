04996027952ffa7c70601c04f81b11acfa57718e
Harry Smith <K1738426@kingston.ac.uk>
2019-01-25 14:06:19 +0000

    Added folders and classes

---
+++ View/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css" class="stylesheet">
+    <title>Berwyn Buses</title>
+</head>
+<body>
+<header>
+<div style="display:inline; width:100%;">
+<h2 style="float:left; text-align:left;">Account<h2>
+<h1>Berwyn Buses</h1>
+<h2 style="float:right;">Vehicles</h2>
+</div>
+</header>
+    
+</body>
+</html>
