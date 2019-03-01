2514cb30e9a5845ebe75d4ca2414ddb6deee1a4e
adiiri12@outlook.com <adiiri12@outlook.com>
2019-03-01 20:00:58 +0000

    alot of git errors

--- Admin/Controller/Vehicle.php
+++ Admin/Controller/Vehicle.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0ff2d9c87eed25234aff6372e5990e9a1535d486
 require_once "../Model/Vehicle.php";
 require_once "../Model/dataAccess.php";
 
@@ -54,83 +50,5 @@ else {
           
       //echo "<br> IF statwement </br>";
      
-       
-<<<<<<< HEAD
-=======
-=======
-
-require_once "../Model/Vehicle.php";
-require_once "../Model/dataAccess.php";
-
-
-
-
-if (!isset($_REQUEST["search"]))
-      {  
-      $results = getAllVehicles();
-          //echo "<br> IF statwement </br>";
-      }
-       else
-       {
-          $price = $_REQUEST["price"];
-          $numberOfPassengers = $_REQUEST["Passengers"];
-          $vehicleName = $_REQUEST["Vehicle"];
-          $licenceRequried = $_REQUEST["licenceRequried"];
-          $results = getVehicleByAllInputs($price,$vehicleName,$numberOfPassengers,$licenceRequried);
-         if($vehicleName == "" && $price == "" && $numberOfPassengers == "" && $licenceRequried == "")
-         {
-            $results = getAllVehicles();           // The user has everything to null
-         }
-         elseif($vehicleName != "" && $price == "" && $numberOfPassengers =="" && $licenceRequried == "" ) 
-         {
-           $results = getVehicleByModel($vehicleName); 
-         }
-         elseif($price !== "" && $vehicleName == "" && $numberOfPassengers == "" && $licenceRequried == "")
-        {
-           $results = getVehicleByPrice($price);
-        }
-         elseif($numberOfPassengers != "" && $vehicleName == "" && $price == "" && $licenceRequried == "")
-        {
-           $results = getVehiclebySeats($numberOfPassengers);
-        }
-       elseif($licenceRequried != "" && $vehicleName == "" && $price == "" && $numberOfPassengers == "")
-        {
-           $results = getVehicleByLicence($licenceRequried);
-        }
-       elseif($numberOfPassengers != "" && $price != "" && $vehicleName == "" && $licenceRequried == "" )
-        {
-           $results =  getVehicleByPriceAndSeats($price,$numberOfPassengers);
-        }
-
-        elseif($numberOfPassengers != "" && $vehicleName != "" && $price == "" && $licenceRequried == "" )
-        {
-            $results = getVehicleByModelAndSeats($vehicleName,$numberOfPassengers);
-        }
-        elseif($licenceRequried != "" && $price != "" && $vehicleName == "" && $numberOfPassengers == "")
-        {
-           $results = getVehicleByPriceAndLicence($price,$licenceRequried);
-        }
-        elseif($vehicleName != "" && $price != "" && $licenceRequried == "" && $numberOfPassengers == "")
-        {
-           $results = getVehicleByModelAndPrice($vehicleName,$price);
-        }
-        elseif($vehicleName != "" && $price != "" && $licenceRequried != "" && $numberOfPassengers == "")
-        {
-           $results = getVehicleByModelAndPriceAndLicenceRequried($vehicleName,$price,$licenceRequried);
-        }
-        elseif($vehicleName != "" && $price != "" && $numberOfPassengers != "" && $licenceRequried == "")
-        {
-           $results = getVehicleByModelAndPriceAndNumberOfPassengers($vehicleName,$price,$numberOfPassengers);
-        }
-        elseif($licenceRequried != "" && $price != "" && $numberOfPassengers != "" && $vehicleName == "")
-        {
-           $results = getVehicleByPriceAndNumberOfPassengersAndLicenceRequried($licenceRequried,$price,$numberOfPassengers);
-        }
-
-    }
-         
-
->>>>>>> 568ba6872863a56fbc980df47582b63fe699af80
->>>>>>> 0ff2d9c87eed25234aff6372e5990e9a1535d486
   require_once "../View/Vehicle.php";
 ?>
