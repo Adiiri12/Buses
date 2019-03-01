72658535797d96fbf293d679e020f24e810ed496
adiiri12@outlook.com <adiiri12@outlook.com>
2019-03-01 20:07:25 +0000

    completed error checking final bits

--- Admin/Model/Vehicle.php
+++ Admin/Model/Vehicle.php
@@ -1,14 +1,8 @@
 <?php
     class Vehicle {
-<<<<<<< HEAD
+
         private $vehicle_id;
-=======
-<<<<<<< HEAD
         private $vehicle_id;
-=======
-        private $id;
->>>>>>> 568ba6872863a56fbc980df47582b63fe699af80
->>>>>>> 0ff2d9c87eed25234aff6372e5990e9a1535d486
         private $vehicleMake;
         private $numberOfSeats;
         private $vehicleType;
