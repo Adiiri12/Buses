3a61380831b8d3620e54fd01002b08c924fe0921
Mohamud <K1744680@kingston.ac.uk>
2019-03-19 15:49:15 +0000

    Finshed the about us page

--- View/About.php
+++ View/About.php
@@ -10,14 +10,8 @@
 </head>
 <body>
 <div class="container my-4">
-<<<<<<< HEAD
 <div class = "row ">
 <div class = "col">
-=======
-<div class = "row">
-<div class = "col-md-6">
-<div class="container">
->>>>>>> fe8583188482d766449cf90a8caef2a18ad4b5c4
 <div class="accordion" id="accordionExample">
   <div class="card">
     <div class="card-header" id="headingOne">
@@ -65,14 +59,7 @@ Berwyn@buseSsales.com General Information, Bus Service Information, Customer Ser
       </div>
     </div>
   </div>
-  <div class = "col-md-6">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div>
+
 </div>
 </div>
 </div>
