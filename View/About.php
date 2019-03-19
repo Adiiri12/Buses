f5f043e964f88fbdd45e96c2531a044f6348f634
Mohamud <K1744680@kingston.ac.uk>
2019-03-19 16:09:39 +0000

    fixed the collapse

--- View/About.php
+++ View/About.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 <div class="container my-4">
-<div class = "row ">
+<div class = "row">
 <div class = "col">
 <div class="accordion" id="accordionExample">
   <div class="card">
@@ -143,48 +143,45 @@ Berwyn@buseSsales.com General Information, Bus Service Information, Customer Ser
 <h5 class="card-header bg-success text-white bg-dark border border-bginfo">FAQ?</h5>
 <div class="accordion" id="accordionExample">
   <div class="card">
-    <div class="card-header" id="headingOne">
+    <div class="card-header" id="aboutus1">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        <button class="btn btn-link collapsed " type="button" data-toggle="collapse" data-target="#c1" aria-expanded="false" aria-controls="c1">
          How to obtain A vehicle ?
         </button>
       </h5>
     </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+    <div id="c1" class="collapse show" aria-labelledby="aboutus1" data-parent="#accordionExample">
       <div class="card-body">
-      Berwyn Bus Company has kept the people of London moving for the past 20 years. Our services, seemingly spans
- the width and breadth of all London we take passengers from Glasgow, Heathrow, Gatwick Airports and Surrey
+      Obtaining a vehicle has bever been easier. You just need to pick what car you want and your ready to go on a journey with your very own berwyn bus.
       </div>
     </div>
   </div>
   <div class="card">
-    <div class="card-header" id="headingTwo">
+    <div class="card-header" id="basket1">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#c2" aria-expanded="false" aria-controls="c2">
           How to add to basket ?
         </button>
       </h5>
     </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+    <div id="c2" class="collapse" aria-labelledby="basket1" data-parent="#accordionExample">
       <div class="card-body">
-      Abdullahi: Our director of delivery service, Abdullahi took up our job mantle in september 2018 after being
- our general manager. Abdullahi had helped berwyn buses go from strength to strength. What makes Abdullahi special is his keen insight and intellect for the tiniest detail and has a strong sense of customer service which helped push berwyn buses reputation. Abdirahman:Our post seems to always be busy at our London station, Abdirahman our service delivery manager took up the mantle to be our delivery manager and eventually has gone one to give our company a good name.
+     You like what you see. Why dont you just put it in your basket and adding to your basket has never been easier by just sinply pressing the add basket button.  
       </div>
     </div>
   </div>
   <div class="card">
-    <div class="card-header" id="headingThree">
+    <div class="card-header" id="signup">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#c3" aria-expanded="false" aria-controls="c3">
         How to sign up ?
         </button>
       </h5>
     </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+    <div id="c3" class="collapse" aria-labelledby="signup" data-parent="#accordionExample">
       <div class="card-body">
-      Contact Us Berwyn Buses Company Kingston Kingston, KT11 4YF, London United Kingdom T: +44 (0)2078 865656 F: +44 (0)1808 864340 Email us Bus Sales Enquries: 
-Berwyn@buseSsales.com General Information, Bus Service Information, Customer Services:customerservices@BerwynBuses.com
+    Signing up with us today has never been easier. Just press the big green button. and join us.  
       </div>
     </div>
   </div>
