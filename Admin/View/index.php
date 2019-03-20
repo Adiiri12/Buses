fddf563cf3fc5b54f245eb0d9a275a96d8368912
adiiri12@outlook.com <adiiri12@outlook.com>
2019-03-20 19:10:53 +0000

    changed the file routing

--- Admin/View/index.php
+++ Admin/View/index.php
@@ -11,7 +11,7 @@ require_once "../Controller/index.php"
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
-  <link rel="stylesheet" href="../View/styles/styl4.css" class="stylesheet">
+  <link rel="stylesheet" href="../View/styles/Style.css" class="stylesheet">
 <script type = "text/javascript" src = "../View/js/edit.js"></script>
 
 </head>
